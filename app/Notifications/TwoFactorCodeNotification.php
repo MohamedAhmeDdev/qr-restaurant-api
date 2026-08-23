9822d3f9f14c58608d21d91b5cd77a1c6f66f092
mohamed ahmed <ma07041705@gmail.com>
2026-08-23 11:28:26 +0300

    remove queue

--- app/Notifications/TwoFactorCodeNotification.php
+++ app/Notifications/TwoFactorCodeNotification.php
@@ -3,11 +3,10 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TwoFactorCodeNotification extends Notification implements ShouldQueue
+class TwoFactorCodeNotification extends Notification
 {
     use Queueable;
 
